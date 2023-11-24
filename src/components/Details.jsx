b91fe4a9060746c7a56c7245ba845b33c954bbcc
vp337247 <vishnu337247@gmail.com>
2023-11-24 10:36:22 +0530

    details modal size fixed

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,4 +1,5 @@
-function Details({ setOpenModal, movie }) {
+function Details({ setOpenModal, movie}) {
+
     
     return (
         <>
@@ -6,16 +7,16 @@ function Details({ setOpenModal, movie }) {
             onClick={() => setOpenModal(false)} />
             <div><img src={'https://image.tmdb.org/t/p/w500' + movie.poster_path} alt={movie.original_title} /></div>
             <div className='absolute w-full flex justify-center items-center z-30 '>
-                <div className=" bg-black/80 p-6 rounded-lg md:w-[50%] lg:w-[40%]">
+                <div className="bg-black/80 p-6 rounded-lg  md:w-[70%] lg:w-[60%]">
                     <div >
                         <div className='text-white font-bold text-3xl'>
                             {movie.original_title}
                         </div>
-                        <div className="flex gap-4 mt-4">
+                        <div className="flex gap-4 mt-3">
                             <div>
                                 <div > {movie.original_language.toUpperCase()} </div>
                                 <div > {movie.origin_country} </div>
-                                <div className='text-white/40 w-[300px]'> {movie.overview} </div>
+                                <div className='text-white/40 w-[400px]'> {movie.overview} </div>
                             </div>
                             <div><img width='250' src={'https://image.tmdb.org/t/p/w500' + movie.poster_path} 
                             alt={movie.original_title} /></div>
--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -10,22 +10,7 @@ function MovieGrid({ results }) {
     setIsOpen(true);
   };
 
-  // Use useEffect to add/remove Tailwind classes to the body when the modal is opened/closed
-  useEffect(() => {
-    // Add Tailwind classes to the body when the modal is open
-    if (isOpen) {
-      document.body.classList.add("overflow-hidden");
-    } else {
-      // Remove Tailwind classes when the modal is closed
-      document.body.classList.remove("overflow-hidden");
-    }
-
-    // Cleanup: Remove the classes when the component unmounts
-    return () => {
-      document.body.classList.remove("overflow-hidden");
-    };
-  }, [isOpen]);
-
+  
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
       {results.length > 0 ? (
